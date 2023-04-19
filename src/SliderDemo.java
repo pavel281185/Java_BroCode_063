0e2f44b43d000f01e8ad8d69cbf1906bd29f65ef
pavel281185 <pavelkvasnicka@gmail.com>
2023-04-19 15:18:41 +0200

    Initial commit

---
+++ src/SliderDemo.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+
+public class SliderDemo implements ChangeListener {
+    JFrame frame;
+    JPanel panel;
+    JLabel label;
+    JSlider slider;
+
+    SliderDemo() {
+        frame = new JFrame("Slider demo");
+        panel = new JPanel();
+        label = new JLabel();
+        slider = new JSlider(0, 100, 50);
+
+        slider.setPreferredSize(new Dimension(400, 200));
+        slider.setPaintTicks(true);
+        slider.setMinorTickSpacing(10);
+        slider.setPaintTrack(true);
+        slider.setMajorTickSpacing(25);
+        slider.setPaintLabels(true);
+        slider.setFont(new Font("MV Boli", Font.PLAIN, 15));
+        slider.setOrientation(SwingConstants.VERTICAL);
+        label.setText("°C = " + slider.getValue());
+        slider.addChangeListener(this);
+
+        panel.add(slider);
+        panel.add(label);
+
+        frame.add(panel);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        label.setText("°C = " + slider.getValue());
+    }
+}
